2347732b96ab452b9592b46d78b24ee91c2b8e7c
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-09-03 17:22:30 +0200

    feat: add reusable Button component with variant support and icons

---
+++ frontend/components/common/Button.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { ButtonProps } from "@/interfaces";
+import { FaArrowRight } from "react-icons/fa6";
+import { FaPlayCircle } from "react-icons/fa";
+
+const Button: React.FC<ButtonProps> = ({
+  title,
+  variant = "primary",
+  onClick,
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      className={`${
+        variant === "primary"
+          ? "bg-gradient-to-r from-blue-800 to-blue-500 text-white"
+          : variant === "secondary"
+          ? "bg-white"
+          : "bg-orange-500"
+      } flex items-center gap-4 font-semibold`}
+    >
+      {title}
+      {variant === "primary" ? (
+        <FaArrowRight size={20} />
+      ) : variant === "secondary" ? (
+        <FaPlayCircle color="#000" size={20} />
+      ) : (
+        ""
+      )}
+    </button>
+  );
+};
+
+export default Button;
