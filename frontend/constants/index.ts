18854cf85997de37c77a3487f7cc3382cca352d4
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-09-03 17:26:07 +0200

    feat: add HERO_STATS constants for hero section stats

---
+++ frontend/constants/index.ts
@@ -0,0 +1,20 @@
+export const HERO_STATS = [
+  {
+    id: 1,
+    statNumber: "1000+",
+    statDescription: "Jobs Posted",
+    variant: "primary",
+  },
+  {
+    id: 2,
+    statNumber: "500+",
+    statDescription: "Workers Hired",
+    variant: "secondary",
+  },
+  {
+    id: 3,
+    statNumber: "R50k+",
+    statDescription: "Earned",
+    variant: "tertiary",
+  },
+];
