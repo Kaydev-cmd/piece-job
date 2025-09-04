9bdee31e912bc4baa90aff726b4c619c83757943
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-09-04 08:09:34 +0200

    feat: extend variant styles with login, signup, and subscribe options

--- frontend/components/common/Button.tsx
+++ frontend/components/common/Button.tsx
@@ -15,8 +15,16 @@ const Button: React.FC<ButtonProps> = ({
         variant === "primary"
           ? "bg-gradient-to-r from-blue-800 to-blue-500 text-white"
           : variant === "secondary"
-          ? "bg-white"
-          : "bg-orange-500"
+          ? "bg-white text-black"
+          : variant === "tertiary"
+          ? "bg-orange-500 text-white"
+          : variant === "login"
+          ? "bg-gradient-to-r from-blue-800 to-blue-500 text-white"
+          : variant === "signup"
+          ? "bg-white text-black"
+          : variant === "subscribe"
+          ? "bg-blue-700 text-white"
+          : "bg-green-500 text-white"
       } flex items-center gap-4 font-semibold`}
     >
       {title}
