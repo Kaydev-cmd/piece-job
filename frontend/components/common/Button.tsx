2d8d71f1bc4a2b79bc2abdf99821233a56f1ca0c
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-09-10 14:09:13 +0200

    feat: add styles and icons for google, jobSeeker, and employer variants

--- frontend/components/common/Button.tsx
+++ frontend/components/common/Button.tsx
@@ -1,21 +1,24 @@
 import React from "react";
 import { ButtonProps } from "@/interfaces";
 import { FaArrowRight } from "react-icons/fa6";
-import { FaPlayCircle } from "react-icons/fa";
+import { FaPlayCircle, FaGoogle } from "react-icons/fa";
+import { GoDotFill } from "react-icons/go";
 
 const Button: React.FC<ButtonProps> = ({
   title,
   variant = "primary",
   onClick,
+  type,
 }) => {
   return (
     <button
       onClick={onClick}
+      type={type}
       className={`${
         variant === "primary"
           ? "bg-gradient-to-r from-blue-800 to-blue-500 text-white"
           : variant === "secondary"
-          ? "bg-white text-black"
+          ? "bg-white text-black flex flex-row-reverse"
           : variant === "tertiary"
           ? "bg-orange-500 text-white"
           : variant === "login"
@@ -24,6 +27,12 @@ const Button: React.FC<ButtonProps> = ({
           ? "bg-white text-lg text-black hover:border-2 border-blue-600"
           : variant === "subscribe"
           ? "bg-blue-700 text-white"
+          : variant === "google"
+          ? "bg-white border border-slate-500 flex flex-row-reverse"
+          : variant === "jobSeeker"
+          ? "joiningAs flex flex-row-reverse focus:border-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          : variant === "employer"
+          ? "joiningAs flex flex-row-reverse focus:border-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
           : "bg-green-500 text-white"
       } flex items-center gap-4 font-semibold`}
     >
@@ -32,6 +41,12 @@ const Button: React.FC<ButtonProps> = ({
         <FaArrowRight size={20} />
       ) : variant === "secondary" ? (
         <FaPlayCircle color="#000" size={20} />
+      ) : variant === "google" ? (
+        <FaGoogle color="#000" size={20} />
+      ) : variant === "jobSeeker" ? (
+        <GoDotFill color="#1447e6" />
+      ) : variant === "employer" ? (
+        <GoDotFill color="#ffa500" />
       ) : (
         ""
       )}
