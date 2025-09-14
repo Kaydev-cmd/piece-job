b0657944535a569c60c6dfd9cae14b87c24a413e
Kaydev-cmd <keamogetswingwako2001@gmail.com>
2025-09-14 16:46:53 +0200

    feat: add RecentPayments component with job summary, price, date, and status pill

---
+++ frontend/components/common/RecentPayments.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { RecentPaymentsCardProps } from "@/interfaces";
+import Pill from "./Pill";
+
+const RecentPayments: React.FC<RecentPaymentsCardProps> = ({
+  id,
+  jobTitle,
+  date,
+  price,
+}) => {
+  return (
+    <div>
+      {/* Summary goes here... */}
+      <div className="flex flex-col gap-2" style={{ padding: "6px" }}>
+        <div className="flex justify-between items-center text-xl">
+          <h2 className="text-slate-800 font-semibold">{jobTitle}</h2>
+          <p className="text-blue-500 font-bold">+R{price}</p>
+        </div>
+        <div className="flex justify-between items-center">
+          <p className="text-slate-500 font-semibold">{date}</p>
+          <Pill title="Completed" variant="default" />
+        </div>
+        <hr className="text-slate-400" style={{ marginTop: "8px" }} />
+      </div>
+    </div>
+  );
+};
+
+export default RecentPayments;
