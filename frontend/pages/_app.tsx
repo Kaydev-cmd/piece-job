2d4cc9251c64e0c5db1d4a5a82227139c343a7ce
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-09-24 10:04:57 +0200

    feat: wrap app with JobPostProvider

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,10 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "@/components/layout/Layout";
+import { JobPostProvider } from "@/context/JobPostContext";
 export default function App({ Component, pageProps }: AppProps) {
-  return(
+  return (
     <Layout>
-      <Component {...pageProps} />
+      <JobPostProvider>
+        <Component {...pageProps} />
+      </JobPostProvider>
     </Layout>
   );
 }
