8c29cdff1161fbdc0594dafd7955a414583fe19e
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-10-02 16:19:12 -0700

    feat: display 'See more jobs' button when job feed has 6 or more items

--- frontend/pages/job-feed/index.tsx
+++ frontend/pages/job-feed/index.tsx
@@ -270,7 +270,7 @@ const JobFeedPage = () => {
       )}
 
       <div className="flex justify-center" style={{ marginTop: "16px" }}>
-        {jobFeed.length > 6 ? (
+        {jobFeed.length >= 6 ? (
           <Button title="See more jobs" variant="seeMore" />
         ) : null}
       </div>
