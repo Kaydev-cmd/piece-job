ae63b2494c9612ec3920f65b1ea2f594b4e21273
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-10-09 11:24:26 -0700

    style: update BusinessInfoCard layout and styling for improved spacing and alignment

--- frontend/components/common/BusinessInfoCard.tsx
+++ frontend/components/common/BusinessInfoCard.tsx
@@ -9,30 +9,26 @@ const BusinessInfoCard: React.FC<JobPosterProfileCardProps> = ({
   activeJobs,
 }) => {
   return (
-    <div className="card flex flex-col  items-center gap-8 justify-center bg-gray-300/30 shadow-md hover:shadow-lg transition-shadow rounded-xl">
-      <h1 className="flex items-center gap-2 text-2xl md:text-3xl font-bold self-start  ">
+    <div className="card flex flex-col gap-6 bg-gray-300/30 rounded-xl">
+      <h1 className="flex items-center gap-2 text-3xl font-bold">
         <FaMedal size={20} color="#22C55E" /> Business Information
       </h1>
-      <div className="flex flex-col items-center gap-2">
-        <h3 className="text-lg md:text-2xl text-muted-foregroundfont  font-semibold ">
-          {businessName}
-        </h3>
-        <p className="text-center px-4 font-semibold "> {biography}</p>
+      <div className="flex flex-col items-center gap-4" style={{marginBottom: "14px"}}>
+        <h3 className="text-xl font-semibold">{businessName}</h3>
+        <p className="text-center"> {biography}</p>
       </div>
-      <div className="flex flex-col text-center md:flex-row gap-10 px-4 hover:underline">
-        <div className="flex flex-col  items-center">
+      <div className="flex flex-col text-center md:flex-row md:justify-center gap-10">
+        <div className="flex flex-col items-center">
           {postedJobs !== undefined && (
-            <p className="text-lg font-bold">{postedJobs}</p>
+            <p className="text-xl font-bold">{postedJobs}</p>
           )}
-          <p className="text-sm text-muted-foreground font-bold">Posted Jobs</p>
+          <p className="font-bold">Posted Jobs</p>
         </div>
         <div className="flex flex-col items-center">
           {activeJobs !== undefined && (
-            <p className="text-lg font-bold">{activeJobs}</p>
+            <p className="text-xl font-bold">{activeJobs}</p>
           )}
-          <p className="text-sm text-muted-foreground  font-bold">
-            Active Jobs
-          </p>
+          <p className="font-bold">Active Jobs</p>
         </div>
       </div>
     </div>
