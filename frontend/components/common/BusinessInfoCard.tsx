8b7413b7d1e03e03601b1abaa8b22044b4fe6acd
Kaydev-cmd <keamogetswingwako2001@gmail.com>
2025-10-10 16:06:17 +0200

    fix: adjust styling issues in BusinessInfoCard component

--- frontend/components/common/BusinessInfoCard.tsx
+++ frontend/components/common/BusinessInfoCard.tsx
@@ -10,7 +10,7 @@ const BusinessInfoCard: React.FC<JobPosterProfileCardProps> = ({
 }) => {
   return (
     <div className="card flex flex-col gap-6 bg-gray-300/30 rounded-xl">
-      <h1 className="flex items-center gap-2 text-3xl font-bold">
+      <h1 className="flex items-center gap-2 text-2xl md:text-3xl font-bold">
         <FaMedal size={20} color="#22C55E" /> Business Information
       </h1>
       <div className="flex flex-col items-center gap-4" style={{marginBottom: "14px"}}>
